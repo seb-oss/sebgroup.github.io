26ac4621d6b9af79a88c03b2e6840304392cac5e
Patrik Eriksson <patrik.eriksson@seb.se>
2020-01-30 16:09:51 +0100

    chore: get token from file

--- data-loaders/project-fetcher.js
+++ data-loaders/project-fetcher.js
@@ -1,15 +1,14 @@
-const { GraphQLClient } = require('graphql-request')
-const fs = require('fs')
-const token = process.env.GITHUB_TOKEN
+const { GraphQLClient } = require("graphql-request")
+const fs = require("fs")
 
-const client = new GraphQLClient('https://api.github.com/graphql', {
-    headers: {
-        Authorization: `Bearer ${token}`
-    }
+const client = new GraphQLClient("https://api.github.com/graphql", {
+  headers: {
+    Authorization: `Bearer ${getToken()}`
+  }
 })
 
 function getProjects() {
-    const query = `{
+  const query = `{
         organization(login: "sebgroup") {
             repositories(first: 100 isFork: false) {
                 edges {
@@ -33,32 +32,49 @@ function getProjects() {
         }
     }`
 
-    client
-        .request(query)
-        .then(e => e.organization.repositories.edges)
-        .then(graphQLToJson)
-        .then(writeToProjectsFile)
+  client
+    .request(query)
+    .then(e => e.organization.repositories.edges)
+    .then(graphQLToJson)
+    .then(writeToProjectsFile)
 }
 
 function writeToProjectsFile(projects) {
-    fs.writeFileSync('../_data/projects.json', JSON.stringify(projects, null, 2))
+  fs.writeFileSync("../_data/projects.json", JSON.stringify(projects, null, 2))
 }
 
 function graphQLToJson(body) {
-    return body
-        .map(e => e.node)
-        .map(e => Object.assign({}, e, {stargazers: e.stargazers.totalCount}))
-        .filter(e => !e.isArchived)
-        .filter(e => !e.isPrivate)
-        .map(({name, description, homepageUrl, url, pushedAt, primaryLanguage, stargazers}) => ({
-            name, 
-            description,
-            homepageUrl,
-            url,
-            pushedAt,
-            primaryLanguage,
-            stargazers
-        }))
+  return body
+    .map(e => e.node)
+    .map(e => Object.assign({}, e, { stargazers: e.stargazers.totalCount }))
+    .filter(e => !e.isArchived)
+    .filter(e => !e.isPrivate)
+    .map(
+      ({
+        name,
+        description,
+        homepageUrl,
+        url,
+        pushedAt,
+        primaryLanguage,
+        stargazers
+      }) => ({
+        name,
+        description: description || null,
+        homepageUrl: homepageUrl || null,
+        url,
+        pushedAt,
+        primaryLanguage: primaryLanguage || null,
+        stargazers
+      })
+    )
+}
+
+function getToken() {
+  return (
+    process.env.GITHUB_TOKEN ||
+    fs.readFileSync(".env", { encoding: "utf-8" }).split("TOKEN=")[1]
+  )
 }
 
 getProjects()
