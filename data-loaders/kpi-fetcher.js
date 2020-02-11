b7b6728fb559a248ec3fe4633b5c1266e57d36c2
Mikael Gråborg <mikael.graborg@seb.se>
2020-02-11 11:48:53 +0100

    style: move functions to bottom of kpi-fetcher

--- data-loaders/kpi-fetcher.js
+++ data-loaders/kpi-fetcher.js
@@ -1,5 +1,35 @@
 const fs = require("fs")
 const datefns = require('date-fns')
+const { projects, contributors } = readToProjectsFile()
+
+const stars = projects.map(e => e.stargazers).reduce((sum, curr) => sum + curr, 0)
+const forks = projects.map(e => e.forkCount).reduce((sum, curr) => sum + curr, 0)
+const openIssues = projects.map(e => e.openIssues).reduce((sum, curr) => sum + curr, 0)
+
+const activeProjects = getActiveProjectsCount()
+
+appendToProjectsFile({
+    stars,
+    forks,
+    openIssues,
+    activeProjects,
+    contributors,
+    month: datefns.format(datefns.subMonths(new Date(), 1), "MMMM"),
+    year: datefns.getYear(new Date())
+})
+
+function getActiveProjectsCount() {
+    const end = new Date()
+    const start = datefns.sub(end, {days: 30})
+    const monthInterval = datefns.eachMonthOfInterval({start, end})
+    const isWithinLastMonth = date =>
+        datefns.isWithinInterval(datefns.parseISO(date), {
+            start: monthInterval[0],
+            end: monthInterval[1]
+        })
+    return projects.filter(e => isWithinLastMonth(e.pushedAt) || isWithinLastMonth(e.lastIssueCreatedAt)).length
+}
+
 function appendToProjectsFile(kpis) {
     const kpisList = readToKpiFile() || []
     kpisList.push(kpis)
@@ -17,33 +47,3 @@ function readToKpiFile() {
     }
     return JSON.parse(fs.readFileSync(path, {encoding: "utf-8"}))
 }
-
-const { projects, contributors } = readToProjectsFile()
-
-const stars = projects.map(e => e.stargazers).reduce((sum, curr) => sum + curr, 0)
-const forks = projects.map(e => e.forkCount).reduce((sum, curr) => sum + curr, 0)
-const openIssues = projects.map(e => e.openIssues).reduce((sum, curr) => sum + curr, 0)
-
-const activeProjects = getActiveProjectsCount()
-
-function getActiveProjectsCount() {
-    const end = new Date()
-    const start = datefns.sub(end, {days: 30})
-    const monthInterval = datefns.eachMonthOfInterval({start, end})
-    const isWithinLastMonth = date =>
-        datefns.isWithinInterval(datefns.parseISO(date), {
-            start: monthInterval[0],
-            end: monthInterval[1]
-        })
-    return projects.filter(e => isWithinLastMonth(e.pushedAt) || isWithinLastMonth(e.lastIssueCreatedAt)).length
-}
-
-appendToProjectsFile({
-    stars,
-    forks,
-    openIssues,
-    activeProjects,
-    contributors,
-    month: datefns.format(datefns.subMonths(new Date(), 1), "MMMM"),
-    year: datefns.getYear(new Date())
-})
