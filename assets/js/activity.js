f1a5fb1f152f16a44f0776f419e548c32c0b0bae
Patrik Eriksson <patrik.eriksson@seb.se>
2020-02-13 14:44:38 +0100

    feat: style activity page

---
+++ assets/js/activity.js
@@ -0,0 +1,188 @@
+---
+---
+
+const starsData = {{ site.data.activity | map: "stars" | split: "" }}
+const forksData = {{ site.data.activity | map: "forks" | split: "" }}
+const openIssuesData = {{ site.data.activity | map: "openIssues" | split: "" }}
+const contributorsData = {{ site.data.activity | map: "contributors" | split: "" }}
+const activeReposData = {{ site.data.activity | map: "activeProjects" | split: "" }}
+const monthsData = {{ site.data.activity | map: "month" | split: "" }}
+
+const monthList = [
+  'Jan',
+  'Feb',
+  'Mar',
+  'Apr',
+  'May',
+  'Jun',
+  'Jul',
+  'Aug',
+  'Sep',
+  'Oct',
+  'Nov',
+  'Dec'
+]
+monthList.push(...monthList)
+
+const [twelveMonthsAgo] = monthsData.slice(-12)
+const startMonth = monthList.findIndex(
+  month => month === twelveMonthsAgo.slice(0, 3)
+)
+const monthLabels = monthList.slice(startMonth, startMonth + 12)
+
+const starsAndForks = document
+  .getElementById('stars-and-forks')
+  .getContext('2d')
+const starsAndForksChart = new Chart(starsAndForks, {
+  type: 'line',
+  data: {
+    labels: monthLabels,
+    datasets: [
+      {
+        label: 'Stars',
+        fill: false,
+        borderColor: '#f8a000',
+        backgroundColor: '#f8a000',
+        data: starsData.slice(-12)
+      },
+      {
+        fill: false,
+        label: 'Forks',
+        borderColor: '#673ab6',
+        backgroundColor: '#673ab6',
+        data: forksData.slice(-12)
+      }
+    ]
+  },
+  options: {
+    scales: {
+      xAxes: [
+        {
+          ticks: {
+            fontSize: 10
+          }
+        }
+      ],
+      yAxes: [
+        {
+          ticks: {
+            fontSize: 10
+          }
+        }
+      ]
+    }
+  }
+})
+
+const openIssues = document.getElementById('open-issues').getContext('2d')
+const openIssuesChart = new Chart(openIssues, {
+  type: 'line',
+  data: {
+    labels: monthLabels,
+    datasets: [
+      {
+        label: 'Open Issues',
+        fill: false,
+        borderColor: '#bb000c',
+        backgroundColor: '#bb000c',
+        data: openIssuesData.slice(-12)
+      }
+    ]
+  },
+  options: {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            min: 0,
+            stepSize: 100,
+            max: 300,
+            fontSize: 10
+          }
+        }
+      ],
+      xAxes: [
+        {
+          ticks: {
+            fontSize: 10
+          }
+        }
+      ]
+    }
+  }
+})
+
+const contributors = document.getElementById('contributors').getContext('2d')
+const contributorsChart = new Chart(contributors, {
+  type: 'line',
+  data: {
+    labels: monthLabels,
+    datasets: [
+      {
+        label: 'Contributors',
+        fill: false,
+        borderColor: '#379d00',
+        backgroundColor: '#379d00',
+        data: contributorsData.slice(-12)
+      }
+    ]
+  },
+  options: {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            min: 0,
+            stepSize: 50,
+            max: 100,
+            fontSize: 10
+          }
+        }
+      ],
+      xAxes: [
+        {
+          ticks: {
+            fontSize: 10
+          }
+        }
+      ]
+    }
+  }
+})
+const activeRepos = document.getElementById('active-repos').getContext('2d')
+const activeReposChart = new Chart(activeRepos, {
+  type: 'line',
+  data: {
+    labels: monthLabels,
+    datasets: [
+      {
+        label: 'Active repositories',
+        fill: false,
+        borderColor: '#007ac7',
+        backgroundColor: '#007ac7',
+        data: activeReposData.slice(-12)
+      }
+    ]
+  },
+  options: {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            min: 0,
+            stepSize: 10,
+            max: 30,
+            fontSize: 10
+          }
+        }
+      ],
+      xAxes: [
+        {
+          ticks: {
+            fontSize: 10
+          }
+        }
+      ]
+    }
+  }
+})
